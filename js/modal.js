b97edae30a28b57aca8ef6ef3bebc64390d0d210
RomanBykov <30844184+RomanBykov@users.noreply.github.com>
2017-09-11 23:43:36 +0300

    Сделал задание по js

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,30 @@
-	
+
 	var buttonModal = document.querySelector(".button-show-search");
 	var popup = document.querySelector(".modal-search");
 	var date = popup.querySelector(".appointment-form-date");
 	var formModal = popup.querySelector("form");
-	var date = popup.querySelectorAll("[name=date]");
+	var dateIn = popup.querySelector("[name=date-in]");
+	var dateOut = popup.querySelector("[name=date-out]");
 	var adults = popup.querySelector("[name=adults]");
 	var children = popup.querySelector("[name=children]");
 	var storage = localStorage.getItem("date");
-	
-			
+
+
 	buttonModal.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		popup.classList.toggle("modal-off");
 		date.focus();
 	});
-	
+
 	formModal.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-		if (!date.value || !adults || !children.value) {
-			console.log("Нужно заполнить всю форму");
+		if (!dateIn.value || !dateOut.value || !adults.value || !children.value) {
+			evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+			popup.classList.add("modal-error");
 		} else {
 			localStorage.setItem("date", date.value);
 			localStorage.setItem("adults", adults.value);
 			localStorage.setItem("children", children.value);
-		}		
+		}
   });
